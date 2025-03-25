cb67d2b6f78d9435ee8231b4594db5af138c7ef7
YahyaOsama2612 <oyahya868@gmail.com>
2025-03-25 14:58:46 +0400

    storedEditionfive

--- src/app/applications/cart/cartSlice.ts
+++ src/app/applications/cart/cartSlice.ts
@@ -7,19 +7,11 @@ import {
 } from "../../../shared/utilits/functions";
 import { RootState } from "../../store";
 
-const saveCartToLocalStorage = (cartItems: IProduct[]) => {
-  try {
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  } catch (e) {
-    console.error("Error saving cart to localStorage", e);
-  }
-};
+
 
 const initialState: CounterState = {
-  cartItems: localStorage.getItem("cartItems") 
-    ? JSON.parse(localStorage.getItem("cartItems") || '[]') 
-    : [],
-};
+  cartItems : [],
+}
 
 const cartSlice = createSlice({
   name: "cart",
@@ -30,26 +22,22 @@ const cartSlice = createSlice({
         state.cartItems,
         actionPayload.payload
       );
-      saveCartToLocalStorage(state.cartItems);
+      
     },
     removeItemToCart: (state, actionPayload: PayloadAction<IProduct>) => {
       state.cartItems = removeItemFromShoppingCart(
         state.cartItems,
         actionPayload.payload
       );
-      saveCartToLocalStorage(state.cartItems);
+     
     },
-    clearCart: (state) => {
-      state.cartItems = [];
-      localStorage.removeItem("cartItems");
-    }
+    
   },
 });
 
 export const { 
   addItemToCart, 
   removeItemToCart, 
-  clearCart 
 } = cartSlice.actions;
 
 export const selectCartItems = (state: RootState) => state.cart;
